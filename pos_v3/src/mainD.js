8a6dc2c6eb71e362e9db736ab48730e8ba67c997
wfsovereign <never_fall@sohu.com>
2015-01-12 11:13:18 +0800

    refactor and use strategy factory realize all strategy(A_D)

--- pos_v3/src/mainD.js
+++ pos_v3/src/mainD.js
@@ -4,9 +4,8 @@
 function printInventoryD(inputs){
     var receipt_items = build_receipt_items_from_inputs(inputs);
     var items = transfer_obj(receipt_items);
-
-    //var calculator= new Calculator(items);
-    //calculator.calculate();
-    var result = build_receipt_result(receipt_items,Strategy_D(items));
+    var factory = new Strategy_factory();
+    var preference_info_obj = factory.select_strategy("createStrategyD",items);
+    var result = build_receipt_result(receipt_items,preference_info_obj);
     console.log(result);
 }
