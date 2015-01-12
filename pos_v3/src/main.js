d404ff47bf366e9ef3c4dc16317398655f82edd8
wfsovereign <never_fall@sohu.com>
2015-01-12 11:07:53 +0800

    refactor strategyA and use strategy factory

--- pos_v3/src/main.js
+++ pos_v3/src/main.js
@@ -4,9 +4,8 @@
 function printInventory(inputs) {
     var receipt_items = build_receipt_items_from_inputs(inputs);
     var items = transfer_obj(receipt_items);
-    //var items_for_calculate = Strategy_A(receipt_items);
-    //var calculator = new Calculator(items_for_calculate);
-    //calculator.calculate();
-    var result = build_receipt_result(receipt_items,Strategy_A(items));
+    var factory = new Strategy_factory();
+    var preference_info_obj = factory("createStrategyA",items);
+    var result = build_receipt_result(receipt_items,preference_info_obj);
     console.log(result);
 }
