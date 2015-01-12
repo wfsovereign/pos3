095011dd42ac4d1b95c21312d2801ff9a4c3e6e6
wfsovereign <never_fall@sohu.com>
2015-01-12 11:11:03 +0800

    refactor strategyC and use strategy factory

--- pos_v3/src/mainC.js
+++ pos_v3/src/mainC.js
@@ -5,7 +5,8 @@
 function printInventoryC(inputs){
     var receipt_items = build_receipt_items_from_inputs(inputs);
     var items = transfer_obj(receipt_items);
-
-    var result = build_receipt_result(receipt_items,Strategy_C(items));
+    var factory = new Strategy_factory();
+    var preference_info_obj = factory.select_strategy("createStrategyC",items);
+    var result = build_receipt_result(receipt_items,preference_info_obj);
     console.log(result);
 }
